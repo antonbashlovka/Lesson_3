ace71ed85ddc9789947a84d247bfb17046ce2b4a
antonbashlovka <anton.bashlovka@gmail.com>
2022-12-09 19:19:49 +0500

    done task 3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Задача 3
+    Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
+    3 -> [1, 8, 27].
+    5 -> [1, 8, 27, 64, 125]
+    Указание: Вывод массива вынести в отдельную функцию.
+*/
+
+double[] fillArray(int num){
+    double[] cubes = new double[num];
+    for (int i = 1; i <= num ; i++){
+        cubes[i-1] = Math.Pow(i, 3);
+    }
+    return cubes;
+}
+
+void printArray(double[] arr){
+    string res = "[";
+    foreach (double value in arr ){
+        res = res + Convert.ToString(value) + ",";
+    }
+    res = res.TrimEnd(new Char[] {',', ' '}) + "]";
+
+    Console.WriteLine(res);
+}
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int intNumber = 0;
+double[] cubeTable = {}; 
+
+// проверка на число
+while (int.TryParse(number, out intNumber) == false ){
+    Console.WriteLine("Ошибка, вы ввели текст, а нужно число");
+    number = Console.ReadLine();   
+}
+
+cubeTable = fillArray(intNumber);
+printArray(cubeTable);
+
